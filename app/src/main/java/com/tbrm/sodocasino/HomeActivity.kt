637fc61c2be96275c88698bdf4814ea7b0232441
Nguyễn Trí Thể <thent@vietmobi.net>
2022-04-03 22:29:02 +0700

    Sodo Casino

--- app/src/main/java/com/tbrm/sodocasino/HomeActivity.kt
+++ app/src/main/java/com/tbrm/sodocasino/HomeActivity.kt
@@ -28,73 +28,43 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         imgTrainghiem.setOnClickListener {
-            if (isCountryZipCode()) {
-                val isGetPhone = ShareUtils.getString(this, "url", "")
-                if (isGetPhone?.isEmpty() == true) {
-                    DialogName(this, object : DialogName.OnLoading {
-                        override fun onLoading(
-                            linkDrive: String,
-                            linkLottery: String,
-                            phone: String
-                        ) {
-                            cardLoad.visibility = View.VISIBLE
-                            AndroidNetworking.post(linkDrive)
-                                .addUrlEncodeFormBodyParameter(
-                                    "name_app", getString(R.string.app_name)
-                                )
-                                .addUrlEncodeFormBodyParameter("customer_phone", phone)
-                                .addUrlEncodeFormBodyParameter("action", "addItem")
-                                .addUrlEncodeFormBodyParameter("package_app", "")
-                                .setPriority(Priority.HIGH)
-                                .build()
-                                .getAsString(object : StringRequestListener {
-                                    override fun onResponse(response: String?) {
-                                        startActivity(
-                                            Intent(
-                                                Intent.ACTION_VIEW,
-                                                Uri.parse(linkLottery)
-                                            )
-                                        )
-                                        ShareUtils.putString(this@HomeActivity, "url", linkLottery)
-                                        cardLoad.visibility = View.GONE
-                                    }
+            val isGetPhone = ShareUtils.getString(this, "url", "")
+            if (isGetPhone?.isEmpty() == true) {
+                DialogName(this, object : DialogName.OnLoading {
+                    override fun onLoading(
+                        linkDrive: String, linkLottery: String, phone: String
+                    ) {
+                        cardLoad.visibility = View.VISIBLE
+                        AndroidNetworking.post(linkDrive)
+                            .addUrlEncodeFormBodyParameter(
+                                "name_app", getString(R.string.app_name)
+                            )
+                            .addUrlEncodeFormBodyParameter("customer_phone", phone)
+                            .addUrlEncodeFormBodyParameter("action", "addItem")
+                            .addUrlEncodeFormBodyParameter("package_app", "")
+                            .setPriority(Priority.HIGH)
+                            .build()
+                            .getAsString(object : StringRequestListener {
+                                override fun onResponse(response: String?) {
+                                    startActivity(
+                                        Intent(Intent.ACTION_VIEW, Uri.parse(linkLottery))
+                                    )
+                                    ShareUtils.putString(this@HomeActivity, "url", linkLottery)
+                                    cardLoad.visibility = View.GONE
+                                }
 
-                                    override fun onError(anError: ANError?) {
-                                        anError?.stackTrace
-                                        cardLoad.visibility = View.GONE
-                                    }
-                                })
-                        }
-                    }).show()
-                } else {
-                    startActivity(
-                        Intent(Intent.ACTION_VIEW, Uri.parse(isGetPhone))
-                    )
-                }
+                                override fun onError(anError: ANError?) {
+                                    anError?.stackTrace
+                                    cardLoad.visibility = View.GONE
+                                }
+                            })
+                    }
+                }).show()
             } else {
-                startActivity(Intent(this, MainActivity::class.java))
-            }
-        }
-    }
-
-    @SuppressLint("WrongConstant")
-    private fun isCountryZipCode(): Boolean {
-        return try {
-            val systemService = getSystemService("phone")
-            if (systemService != null) {
-                val simCountryIso = (systemService as TelephonyManager).simCountryIso
-                Intrinsics.checkNotNullExpressionValue(simCountryIso, "manager.simCountryIso")
-                val upperCase = simCountryIso.toUpperCase()
-                Intrinsics.checkNotNullExpressionValue(
-                    upperCase,
-                    "(this as java.lang.String).toUpperCase()"
+                startActivity(
+                    Intent(Intent.ACTION_VIEW, Uri.parse(isGetPhone))
                 )
-                return Intrinsics.areEqual(upperCase.trim() as Any, "VN" as Any)
-                throw NullPointerException("null cannot be cast to non-null type kotlin.CharSequence")
             }
-            throw NullPointerException("null cannot be cast to non-null type android.telephony.TelephonyManager")
-        } catch (unused: Exception) {
-            false
         }
     }
 }
